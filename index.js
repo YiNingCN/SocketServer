6bc92dddec74ff7fbef2493c305c2f7e9f615632
gityining <xstar@qq.com>
2014-11-03 16:11:27 +0800

    增加user

--- index.js
+++ index.js
@@ -1,19 +1,77 @@
-var io = require('socket.io')(1337);
-var redis = require('socket.io-redis');
-io.adapter(redis({ host: 'localhost', port: 6379 }));
-io.pingTimeout=0;
+var redis = require('socket.io-redis'),
+  mongoose = require('mongoose'),
+  models = require('./model/models'),
+  server = require('socket.io')(80);
 
-var count =0;
+server.adapter(redis({ host: 'localhost', port: 6379 }));
 
-io.on('connection', function(socket){
-  count++;
-  console.log(count + ':' + socket.id);
-  socket.on('disconnect', function(){
-    count--;
-    console.log(count);
+var user = new models.User({
+  username:'yining2',
+  password:'k593dfpz',
+  email:'xstar@qq.com'
+});
+user.save(function(err){
+  if(err){
+    return console.log(err.msg);
+  }
+
+  models.User.auth('yining','k593dfpz', function(err, user, reason){
+    if(err){
+      return console.log(err.msg);
+    }
+    if(user){
+      return console.log('Login success');
+    }
+    console.log(reason);
   });
-  socket.on('message', function(){
-
-  })
 });
 
+mongoose.connect('mongodb://localhost/socket_server');
+//io.engine.pingTimeout=30000000;
+var count = 0;
+server.on('connection', function(socket){
+  count++;
+  console.log('Client Connected:'+count);
+  socket.on('message', function(data){
+    socket.broadcast.emit('server_message',data);
+    socket.emit('server_message',data);
+  });
+  socket.on('disconnect', function(){
+    count--;
+    console.log('Client Disconnected:'+count);
+  });
+});
+
+
+
+
+
+//var http = require('http').Server(app);
+
+//
+//var redis = require('socket.io-redis');
+//io.adapter(redis({ host: 'localhost', port: 6379 }));
+//
+//app.get('/', function(req, res){
+//  res.send("hiya");
+//});
+//
+//io.on('connection', function(socket){
+//  console.log('a user connected');
+//});
+//
+//http.listen(1337, function(err){
+//  if (err){
+//    console.log(err.message);
+//    return;
+//  }
+//  console.log('listening on *:1337');
+//});
+
+
+
+//io.engine.pingTimeout=300000;
+//
+
+
+//console.log('Socket server is listening on port' + io.port);
